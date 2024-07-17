b0e37d0316ff789c978d6a10334d30ac3622774d
Chigozirim victor <chigozirimvictorasogwa@gmail.com>
2024-07-17 12:22:27 -0700

    initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react' 
+import './Navbar.css'
+import cart_icon from '../Assets/cart_icon.png'
+import cart_img from '../Assets/cart_img.png'
+import search_icon from '../Assets/search_icon.webp'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+const localCart = JSON.parse(localStorage.getItem("cart"))
+const cartCount = localCart ?  localCart.length : 0
+
+// console.log(localCart.length)
+const[menu,setMenu] = useState("Home");
+    return (
+
+       
+        <div className='navbar'>
+
+            <div className='nav'>
+            <img src={cart_img} alt="" />
+            </div>
+            <div className='second-div'>
+
+            
+        
+
+          <div className='nav-logo'>
+            <h2>Bandage</h2>
+          </div>
+
+          <label class="toggle-button" for="menubar">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </label>
+
+        <input className='input_check' type='checkbox' id='menubar'></input>
+        <div className='Navbar'>
+          <ul className='nav-menu'>
+            <li onClick={()=>{setMenu("Home")}}><Link style={{textDecoration: 'none'}}  to='/'>Home</Link> {menu=== "Home"?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu("Products")}}><Link style={{textDecoration: 'none'}} to='/products'>Shop/Products</Link>{menu=== "Products"?<hr />:<></>}</li>
+            <li onClick={()=>{setMenu("About")}}><Link style={{textDecoration: 'none'}} to='/About'>About</Link>{menu=== "About"?<hr />:<></>}</li>
+            <li>Blog</li>
+            <li>Contact</li>
+            <li>Pages</li>
+
+          </ul>
+          <div className='nav-login-cart'>
+           <Link to='/login'><button>Login/Sign Up</button></Link> 
+           <Link to='/cart'> <img src={cart_icon} alt="" /></Link> 
+            <div className='nav-cart-count'>{localCart.length}</div>
+            
+          </div>
+          <div className='icon'><img src={cartCount} alt="" /></div>
+
+          </div>
+          </div>
+        </div>
+        
+    
+
+
+    )
+}
+
+export default Navbar
---
+++ src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,20 @@
+import "../NewCollections/NewCollections.css";
+
+function NewCollections() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <h6>Designing Better Experience</h6>
+        <h2>Problems trying to resolve the conflict between </h2>
+        <p>
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics:
+        </p>
+        <h3>$16.48</h3>
+        <button>ADD YOUR CALL TO ACTION</button>
+      </div>
+    </div>
+  );
+}
+
+export default NewCollections;
